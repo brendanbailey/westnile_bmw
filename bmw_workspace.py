037c06d420aa74f20cf97985116df5fc4b153f3c
Brendan Bailey <brendan.e.bailey@gmail.com>
2017-04-22 17:03:36 -0400

    Adding random forest

--- bmw_workspace.py
+++ bmw_workspace.py
@@ -8,6 +8,10 @@ Created on Mon Apr 17 14:00:59 2017
 import pandas as pd
 import numpy as np
 from geopy.distance import vincenty
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def eda(dataframe): #Performs exploratory data analysis on the dataframe
     print "columns \n", dataframe.columns
@@ -34,6 +38,12 @@ def clean_and_transform(train, spray, weather):
     
     #Imputing 7:44:32 PM because that time has the most frequency and they all occur on 2011-09-07. Same date as when the null values are.
     spray.Time.fillna("7:44:32 PM", inplace = True)
+    
+    #Imputing Data for WetBulb and StnPressure
+    wetbulb_median = np.median(weather_df.dropna(subset = ["WetBulb"]).WetBulb)
+    stnpressure_median = np.median(weather_df.dropna(subset = ["StnPressure"]).StnPressure)
+    weather_df.WetBulb.fillna(np.median(wetbulb_median), inplace = True)
+    weather_df.StnPressure.fillna(np.median(stnpressure_median), inplace = True)
     
     #Originally I tried using dataframe.iterows to add in spray data. That was taking so long. Using a dictionary because that can be more efficient.
     spray_dictionary = {}
@@ -86,9 +96,42 @@ def clean_and_transform(train, spray, weather):
     
     #Converting Date from string to date. The df only has data for odd years which is weird!
     master_df["Date"] = pd.to_datetime(master_df.Date)
+    
     return master_df, spray_dictionary
+
+def evaluate_model(y_true, y_predicted):
+    a_score = accuracy_score(y_true, y_predicted)
+    c_matrix = confusion_matrix(y_true, y_predicted)
+    confusion = pd.DataFrame(c_matrix, index=['y_true', 'y_false'], columns=['predicted_true','predicted_false'])
+    c_report = classification_report(y_true, y_predicted)
+    return a_score, confusion, c_report
 
 train_df = pd.read_csv("assets/train.csv")
 weather_df = pd.read_csv("assets/weather.csv")
 spray_df = pd.read_csv("assets/spray.csv")
 master_df, spray_dictionary = clean_and_transform(train_df, spray_df, weather_df)
+
+#Creating Random Forest
+y = master_df.WnvPresentAdj
+X = master_df[["Block", "Tmax", "Tmin", "Tavg", "Depart", "DewPoint", "WetBulb", "Heat", "Cool", "Sunrise", "Sunset", "StnPressure", "SeaLevel", "ResultSpeed", "ResultDir", "AvgSpeed"]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, test_size=0.3, random_state = 34198)
+max_depth = [1,2,3,4,5]
+max_features = [0.25, 0.5, 0.75]
+forest = RandomForestClassifier(random_state = 98574, n_estimators = 50)
+grid = GridSearchCV(forest, param_grid = {'max_depth': max_depth, 'max_features': max_features}, cv = 10, verbose = True, n_jobs = -1)
+grid.fit(X_train, y_train)
+print grid.score(X_train, y_train)
+print grid.score(X_test, y_test)
+#Best Score 0.91705354379449089
+#Best Params {'max_depth': 5, 'max_features': 0.5}
+
+
+forest = RandomForestClassifier(random_state = 98574, n_estimators = 50, max_depth = 5, max_features = 0.5)
+forest.fit(X_train, y_train)
+print forest.score(X_train, y_train)
+print forest.score(X_test, y_test)
+predictions = forest.predict(X_test)
+a_score, confusion, c_report = evaluate_model(y_test, predictions) #Confusion Matrix Printing Out Wrong
+print a_score
+print confusion
+print c_report
